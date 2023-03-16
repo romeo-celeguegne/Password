725554ebf060cfa84c2c351e54c901faec500e1e
romeo-celeguegne <115151978+romeo-celeguegne@users.noreply.github.com>
2023-03-16 11:06:48 +0100

    Rendu Password ShortTrack

--- password.py
+++ password.py
@@ -1,27 +1,22 @@
-import re  # Import la librairie “Re”
-import hashlib  # Import la librairie "Hashlib"
-while True:  # Tant que
-    mdp = input("Inserer mot de passe: ")
-    if (len(mdp) <= 8):  # mdp inférieur à 8
+import re
+import hashlib
+while True:
+    mdp = input("Veuillez inserer un mot de passe valide: ")
+    if (len(mdp) < 8):
         print("Mot de passe invalide")
-    # mdp recherche et ne trouve pas de lettre minuscule entre a et z
     elif not re.search("[a-z]", mdp):
         print("Mot de passe invalide")
-    # mdp recherche et ne trouve pas de lettre majuscule entre A et Z
     elif not re.search("[A-Z]", mdp):
         print("Mot de passe invalide")
-    # mdp recherche et ne trouve pas de chiffre entre 0 et 9
+    elif not re.search("[@*.?$]", mdp):
+        print("Mot de passe invalide")
     elif not re.search("[0-9]", mdp):
         print("Mot de passe invalide")
-    # mdp recherche et ne trouve pas de caractère spéciale
-    elif not re.search("[._@$]", mdp):
-        print("Mot de passe invalide")
-    else:  # sinon, mot de passe valide
-        print("Mot de passe valide")
-        break  # stop la boucle
+    else:
+        print("Bienvenue")
+        break
 
-encode = mdp.encode('utf-8')  # encode grâce à utf-8
-maj_encode = hashlib.sha256()  # implante hashlib avec sha256
-maj_encode.update(encode)  # implante maj encode avec utf-8
-# dévoile le mdp crypté en hexadecimal
+encode = mdp.encode('utf-8')
+maj_encode = hashlib.sha256()
+maj_encode.update(encode)
 print("Mot de passe crypté: " + maj_encode.hexdigest())
